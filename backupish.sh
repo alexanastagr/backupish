39e29e0a7c4a1b5e352aa4110d7d08a20e4fe3c3
alexanastagr <alexanastagr@gmail.com>
2024-12-18 22:51:57 +0200

    check if databases list file exists

--- backupish.sh
+++ backupish.sh
@@ -4,12 +4,17 @@ DB_USER="your_username"
 DB_PASSWORD="your_password"      
 DB_HOST="localhost"               
 BACKUP_DIR="databases" 
-DATABASES=("db1" "db2" "db3")       
+DB_LIST="databases.txt"     
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")   
 
 # DO NOT CHANGE ANYTHING BELOW THIS LINE 
 
 mkdir -p "$BACKUP_DIR"
+
+if [ ! -f "$DB_LIST" ]; then
+    echo "File $DB_LIST not found! Check the file path." >&2
+    exit 1
+fi
 
 echo "💾 Starting the backup proccess...."
 
